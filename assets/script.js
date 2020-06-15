fd39f3156ebff00cdbf8c033813b208a970f9abf
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-15 17:58:35 -0400

    hour applied to columns

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,31 @@
 //VARIABLES
 var currentTime = "";
-var currentDate = "";
+var currentDate = moment().format("dddd, MMMM Do");
 var currentHour = "";
 
 var arrayOfHours = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", ]
 
-for (i = 0; i < arrayOfHours.length; i++) {
-    createHourEntry();
+//FUNCTION DEFINITIONS
+
+function displayDate() {
+    $("#currentDay").text(currentDate);
+}
+
+function createSchedule() {
+    for (i = 0; i < arrayOfHours.length; i++) {
+        createHourEntry();
+    }
 }
 
 function createHourEntry() {
-    var hourColumn = ($("<div>").attr("class", "col-1 hour")).append($("<p>" + "TEXT" + "</p>").attr("class", "time-block-text"))
-    var contentColumn = ($("<div>").attr("class", "col-10 time-block content")).append($("<p>" + "TEXT" + "</p>").attr("class", "time-block-text"))
+    var hourText = arrayOfHours[i];
+    var hourColumn = ($("<div>").attr("class", "col-2 hour")).append($("<p>" + hourText + "</p>").attr("class", "text"));
+    var contentColumn = ($("<textarea>").attr("class", "col-10 time-block")).append($("<p>" + "TEXT" + "</p>").attr("class", "text"));
     
     var row = $("<div>").attr("class", "row time-block present").attr("id", "time-block-row").append(hourColumn, contentColumn);
     $("#schedule").append(row);
 }
 
-// var hourColumn = ($("<div>").attr("class", "col-1 hour")).append($("<p>" + "TEXT" + "</p>").attr("class", "time-block-text"))
-// var contentColumn = ($("<div>").attr("class", "col-10 time-block content")).append($("<p>" + "TEXT" + "</p>").attr("class", "time-block-text"))
-
-// var row = $("<div>").attr("class", "row time-block present").attr("id", "time-block-row").append(hourColumn, contentColumn);
-// $("#schedule").append(row);
-
-
-// $("#schedule").append($("<div>").attr("class", "row time-block present").attr("id", "time-block-row"))
-// $("#time-block-row").append($("<div>").attr("class", "col-1 hour"));
-// $("#time-block-row").append($("<div>").attr("class", "col-10 time-block content"));
-// $(".hour").append($("<p>" + "TEXT" + "</p>").attr("class", "time-block-text"))
-// $(".content").append($("<p>" + "TEXT" + "</p>").attr("class", "time-block-text"))
-
-// $("#buttons-view").append($("<button>" + movies[i] + "</button>"))
+//FUNCTION CALLS
+displayDate();
+createSchedule();
