347494506428258c95a080f94683b3b29148a51e
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-06-15 20:12:25 -0400

    color changes for each block based on the current time

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,13 @@
 //VARIABLES
-var currentDate = moment().format("dddd, MMMM Do");
-var currentHour = moment().format("h");
-var adjustedHour = "";
+var currentDate = moment().format("dddd, MMMM Do, LT");
+var currentHour = moment().format("k");
 console.log(currentHour);
-
+var row = "";
 
 var arrayOfHours = ["9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", ]
+var numericHourArray = [9, 10, 11, 12, 13, 14, 15, 16, 17,]
 
 //FUNCTION DEFINITIONS
-
 function displayDate() {
     for (i = 0; i < arrayOfHours.length; i++) {
     $("#currentDay").text(currentDate);
@@ -16,31 +15,36 @@ function displayDate() {
     }
 }
 
-function getHour() {
-    if (currentHour > 9) {
-        adjustedHour = currentHour + ":00 PM";
-    } else {
-        adjustedHour = currentHour + ":00 AM";
+function setColor() {
+    if (numericHourArray[i] === currentHour) {
+        row.attr("class", "row time-block present");
+    }else if (numericHourArray[i] < currentHour) {
+        row.attr("class", "row time-block past");
+    }else {
+        row.attr("class", "row time-block future");
     }
 }
 
 function createHourEntry() {
-    getHour();
-    var hourText = arrayOfHours[i]
+    hourText = arrayOfHours[i]
     var hourColumn = ($("<div>").attr("class", "col-2 hour")).append($("<p>" + hourText + "</p>").attr("class", "text"))
     var contentColumn = ($("<textarea>").attr("class", "col-9 time-block content")).append($("<p>").attr("class", "text"))
     var saveColumn = ($("<div>").attr("class", "col-1 saveBtn")).append($("<button>").attr("class", "btn fas fa-save"))
-
-    var row = $("<div>").attr("class", "row time-block past").attr("id", "time-block-row").append(hourColumn, contentColumn, saveColumn);
+    
+    row = $("<div>").attr("class", "row time-block").append(hourColumn, contentColumn, saveColumn);
+    row.attr("class", "row time-block");
+    
+    setColor();
+    
     $("#schedule").append(row);
 }
 
 //FUNCTION CALLS
 displayDate();
-getHour();
 // createSchedule();
 
 $("button").on("click", function () {
+    console.log('You clicked a button!');
 })
 
 // // $(".btn").on("click", function () {
@@ -48,11 +52,3 @@ $("button").on("click", function () {
 // //     var textId = buttonId.replace("b", "a");
 // //     var textContent = $("#" + textId).children()[0].value;
 // //     localStorage.setItem(textId, textContent);
-
-// if (hour === currentHour) {
-//     $("#" + hour).css("background-color", "#ff6961;");
-// } else if (hour < currentHour) {
-//     $("#" + hour).css("background-color", "#d3d3d3");
-// } else {
-//     $("#" + hour).css("background-color", "#77dd77");
-// }
